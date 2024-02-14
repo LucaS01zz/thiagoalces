2df29c0f61c65e06b8984d9068286e2b79498bc8
LucaS01zz <143469557+LucaS01zz@users.noreply.github.com>
2024-02-14 09:21:25 -0300

    Add files via upload

---
+++ jsv/logica-js-projeto_inicial/desafios.js
@@ -0,0 +1,10 @@
+diasemana = prompt('Qual o dia?')
+if (diasemana == 'sabado') {
+    alert('Bom fim de semana!');
+} else if (diasemana == 'domingo'){
+    alert('Bom fim de semana!')
+} else{
+    if(numeroSecreto > chute){
+        alert('O numero é menor do que ',chute)
+    }
+}
